38efa01466ee63f3666b9614124d419b6b29cdf3
Yilun Zhao <yl4zhao@uwaterloo.ca>
2019-01-26 09:58:14 -0800

    Adding foundation for Islands. New method to place entity on island

--- GGJ2019/Assets/Script/Entity/Entity.cs
+++ GGJ2019/Assets/Script/Entity/Entity.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public enum EntityDirection
 {
-    NONE,
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
+    NONE = -1,
+    UP = 0,             //Up is default for entity direction
+    DOWN = 1,
+    LEFT = 2,
+    RIGHT = 3
 };
 
 public class Entity : MonoBehaviour {
@@ -21,6 +21,33 @@ public class Entity : MonoBehaviour {
 
     public void SetSortingOrder(int order) {
         GetComponent<SpriteRenderer>().sortingOrder = order;
+    }
+
+    public Vector2[] GetDirectionalOccupancy() {
+        List<Vector2> rotated = new List<Vector2>();
+        if(direction == EntityDirection.UP) {
+            return occupancy;
+        }
+        else if(direction == EntityDirection.DOWN) {
+            foreach(Vector2 pos in occupancy) {
+                rotated.Add(new Vector2(pos.x * -1, pos.y * -1));
+            }
+        }
+        else if(direction == EntityDirection.RIGHT) {
+            foreach(Vector2 pos in occupancy) {
+                rotated.Add(new Vector2(pos.y, pos.x * -1));
+            }   
+        }
+        else if(direction == EntityDirection.LEFT) {
+            foreach(Vector2 pos in occupancy) {
+                rotated.Add(new Vector2(pos.y * -1, pos.x));
+            }   
+        }
+        else {
+            return occupancy; //TODO: This should not happen
+        }
+
+        return rotated.ToArray();
     }
 
     public virtual void Interact(/*Player player,*/ EntityDirection dir = EntityDirection.NONE) { }
---
+++ GGJ2019/Assets/Script/Map/Island.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum IslandType {
+	FreeForm,
+	GridLocked,
+}
+
+public class Island : MonoBehaviour {
+	public Vector2 coordinate;
+	public Dictionary<Vector2, Tile> tiles;
+	public IslandType type;
+	public Rigidbody2D rb;
+
+	void Start() {
+		tiles = new Dictionary<Vector2, Tile>();
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	public bool IsEntityPlaceable(Entity entity, Vector2 pivot) {
+		foreach(Vector2 occupant in entity.GetDirectionalOccupancy()) {
+			if(!tiles.ContainsKey(occupant + pivot)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void PlaceEntity(Entity entity, Vector pivot) {
+		foreach(Vector2 occupant in entity.GetDirectionalOccupancy()) {
+			tiles[occupant + pivot].PlaceEntity(entity, occupant);
+		}
+	}
+}
