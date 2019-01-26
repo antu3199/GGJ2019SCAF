5a7265bb4bd5d1bcb202fbbc944de6fa09ace2d1
Leo <lileo.jh+github@gmail.com>
2019-01-26 18:33:27 -0500

    Added island gen

--- GGJ2019/Assets/Script/Map/Island.cs
+++ GGJ2019/Assets/Script/Map/Island.cs
@@ -14,8 +14,11 @@ public class Island : MonoBehaviour {
 	public Rigidbody2D rb;
 	public Map map;
 
-	void Start() {
+	void Awake() {
 		tiles = new Dictionary<Vector2, Tile>();
+	}
+
+	void Start() {
 		rb = GetComponent<Rigidbody2D>();
 		UpdateChildTiles();
 	}
