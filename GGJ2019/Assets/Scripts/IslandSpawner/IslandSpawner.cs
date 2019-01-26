688265bea1c8389c50c83505916f92cf461412bc
Leo <lileo.jh+github@gmail.com>
2019-01-26 18:33:27 -0500

    Added island gen

--- GGJ2019/Assets/Scripts/IslandSpawner/IslandSpawner.cs
+++ GGJ2019/Assets/Scripts/IslandSpawner/IslandSpawner.cs
@@ -6,7 +6,7 @@ public class IslandSpawner : MonoBehaviour {
 
 	private Camera cam;
 	public GameObject homeIsland;		// Reference to player's island.
-	public GameObject[] islandPrefabs;
+	public IslandGenerator islandGenerator;
 	public float homeBuffer;			// The closest distance an island can pass by the home island.
 	public RandomValue spawnCooldown;
 	public RandomValue islandVelocity;
@@ -20,7 +20,7 @@ public class IslandSpawner : MonoBehaviour {
 
 	public void StartSpawning()
 	{
-		if (islandPrefabs.Length > 0) {
+		if (islandGenerator.tilePrefabs.Length > 0) {
 			StartCoroutine(SpawnIslands());
 		}
 		else {
@@ -31,16 +31,15 @@ public class IslandSpawner : MonoBehaviour {
 	IEnumerator SpawnIslands()
 	{
 		while (true) {
-			GameObject chosenPrefab = islandPrefabs[Random.Range(0, islandPrefabs.Length)];
 			float halfCamHeight = cam.orthographicSize;
 			float halfCamWidth = halfCamHeight * cam.aspect;
+
+			// Get generated island object
+			GameObject island = islandGenerator.GenerateIsland();
 
 			// Randomly choose whether this island is vertically-moving or horizontally-moving
 			if (Random.value < 0.5f) {
 				// Vertically-moving island
-					
-				// Initialize object outside of camera
-				GameObject island = Instantiate(chosenPrefab, new Vector3(cam.transform.position.x - 4 * halfCamHeight * cam.aspect, 0, 0), Quaternion.identity);
 				RandomValue spawnXRange = new RandomValue(cam.transform.position.x - halfCamWidth, cam.transform.position.x + halfCamWidth);
 				spawnXRange.SetExcludedRange(new Range(GetBounds(homeIsland).min.x - homeBuffer - GetBounds(island).extents.x, GetBounds(homeIsland).max.x + homeBuffer + GetBounds(island).extents.x));
 				float newIslandX = spawnXRange.GetRandom();
@@ -48,22 +47,23 @@ public class IslandSpawner : MonoBehaviour {
 				if (Random.value < 0.5f) {
 					// Island spawns from top and moves down
 					float newIslandY = cam.transform.position.y + halfCamHeight + GetBounds(island).extents.y;
-					Vector3 newIslandPosition = new Vector3(newIslandX, newIslandY, chosenPrefab.transform.position.z);
+					Vector3 newIslandPosition = new Vector3(newIslandX, newIslandY, island.transform.position.z);
 					// Move island to randomly generated position
 					island.transform.position = newIslandPosition;
+					Debug.Log("spawning at " + newIslandPosition);
+
 					island.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -islandVelocity.GetRandom());
 				} else {
 					// Island spawns from bottom and moves up
 					float newIslandY = cam.transform.position.y - halfCamHeight - GetBounds(island).extents.y;
-					Vector3 newIslandPosition = new Vector3(newIslandX, newIslandY, chosenPrefab.transform.position.z);
+					Vector3 newIslandPosition = new Vector3(newIslandX, newIslandY, island.transform.position.z);
+					Debug.Log("spawning at " + newIslandPosition);
+
 					island.transform.position = newIslandPosition;
 					island.GetComponent<Rigidbody2D>().velocity = new Vector2(0, islandVelocity.GetRandom());
 				}
 			} else {
 				// Horizontally-moving island
-
-				// Initialize object outside of camera
-				GameObject island = Instantiate(chosenPrefab, new Vector3(cam.transform.position.x - 4 * halfCamHeight * cam.aspect, 0, 0), Quaternion.identity);
 				RandomValue spawnYRange = new RandomValue(cam.transform.position.y - halfCamHeight, cam.transform.position.y + halfCamHeight);
 				spawnYRange.SetExcludedRange(new Range(GetBounds(homeIsland).min.y - homeBuffer - GetBounds(island).extents.y, GetBounds(homeIsland).max.y + homeBuffer + GetBounds(island).extents.y));
 				float newIslandY = spawnYRange.GetRandom();
@@ -72,7 +72,7 @@ public class IslandSpawner : MonoBehaviour {
 				{
 					// Island spawns from left and moves right
 					float newIslandX = cam.transform.position.x + halfCamWidth + GetBounds(island).extents.x;
-					Vector3 newIslandPosition = new Vector3(newIslandX, newIslandY, chosenPrefab.transform.position.z);
+					Vector3 newIslandPosition = new Vector3(newIslandX, newIslandY, island.transform.position.z);
 					island.transform.position = newIslandPosition;
 					island.GetComponent<Rigidbody2D>().velocity = new Vector2(-islandVelocity.GetRandom(), 0);
 				}
@@ -80,7 +80,7 @@ public class IslandSpawner : MonoBehaviour {
 				{
 					// Island spawns from right and moves left
 					float newIslandX = cam.transform.position.x - halfCamWidth - GetBounds(island).extents.x;
-					Vector3 newIslandPosition = new Vector3(newIslandX, newIslandY, chosenPrefab.transform.position.z);
+					Vector3 newIslandPosition = new Vector3(newIslandX, newIslandY, island.transform.position.z);
 					island.transform.position = newIslandPosition;
 					island.GetComponent<Rigidbody2D>().velocity = new Vector2(islandVelocity.GetRandom(), 0);
 				}
