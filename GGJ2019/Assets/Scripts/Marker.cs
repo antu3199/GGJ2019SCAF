eaf5391197a67f7458b3c1d73f5b69b05104f2b1
nansonzheng <nanson.zheng@mail.utoronto.ca>
2019-01-26 05:00:58 -0500

    Player character groundwork

---
+++ GGJ2019/Assets/Scripts/Marker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marker : MonoBehaviour {
+    // Object class for indicating which tile the player will be interacting with
+
+    // public Tile selectedTile;
+    // public Tile[4] tiles;
+    CircleCollider2D circle;
+    public ContactFilter2D tileFilter;
+
+	// Use this for initialization
+	void Start () {
+        // markers must be child of some gameobject
+		if (transform.parent == null)
+        {
+            Destroy(gameObject);
+        }
+        circle = GetComponent<CircleCollider2D>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Collider2D[] overlaps = new Collider2D[4];
+        circle.OverlapCollider(tileFilter, overlaps);
+    
+        // TODO Add in first tile if selectedTile is empty 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Collider2D[] overlaps = new Collider2D[4];
+        circle.OverlapCollider(tileFilter, overlaps);
+
+        // TODO set new tile as selectedTile if old selectedTile is out
+        /* if (collision.GetComponent<Tile>() == selectedTile){
+        *      // choose one from existing list
+        *      selectedTile = overlaps[0].GetComponent<Tile>();
+        *  }
+        */
+    }
+}
---
+++ GGJ2019/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+    public float moveSpeed;
+
+    Rigidbody2D r;
+    Marker marker;
+    
+	// Use this for initialization
+	public override void Start () {
+        base.Start();
+        r = GetComponent<Rigidbody2D>();
+        if (!(marker = GetComponent<Marker>())){
+            Debug.LogError("Player must have a marker attached!");
+        }
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+        // Controls
+        r.velocity = moveSpeed * new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+	}
+}
