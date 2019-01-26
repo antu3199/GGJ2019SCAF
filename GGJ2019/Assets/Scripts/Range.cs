37eeb7e087c43f0aa2ed4536af6b0ee12a6c2c2a
Leo <lileo.jh+github@gmail.com>
2019-01-26 05:27:59 -0500

    Created island spawner

---
+++ GGJ2019/Assets/Scripts/Range.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Range {
+	public float min;
+	public float max;
+
+	public Range(float min, float max)
+	{
+		this.min = min;
+		this.max = max;
+	}
+}
