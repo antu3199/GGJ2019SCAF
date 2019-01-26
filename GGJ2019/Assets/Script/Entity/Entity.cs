f1f3a6236adfddb9301e0dabf438e98fbbd366c2
Yilun Zhao <yl4zhao@uwaterloo.ca>
2019-01-26 02:31:31 -0800

    Merging Tiles with Entities

---
+++ GGJ2019/Assets/Script/Entity/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EntityDirection
+{
+    NONE,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+public class Entity : MonoBehaviour {
+    //public Vector2 pivotPosition; //TODO: If we assume all pivots are on 0,0 (bottom-left) we may not need this
+    //public List<Tile> tileEntities; //TODO: Try to avoid this reference as possible
+    public Vector2[] occupancy;
+    public bool interactable;
+    public EntityDirection direction;
+
+    public void SetSortingOrder(int order) {
+        GetComponent<SpriteRenderer>().sortingOrder = order;
+    }
+
+    public virtual void Interact(/*Player player,*/ EntityDirection dir = EntityDirection.NONE) { }
+
+    void Start() { }
+    void Update() { }
+}
---
+++ GGJ2019/Assets/Script/Map/Tile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType
+{
+    GHOST = 0,
+    OWNED = 1,
+    UNOWNED = 2
+}
+
+public class EntityRef {
+    public Entity entity;
+    public Vector2 location;
+
+    public EntityRef(Entity entity, Vector2 offset) {
+        this.entity = entity;
+        this.location = offset;
+    }
+}
+
+public class Tile : MonoBehaviour {
+    public TileType tileType;
+    public Vector2 coordinate;
+    public EntityRef entityRef;
+
+    public int GetSortingOrder() {
+        return (int)(-coordinate.y * 100);
+    }
+
+    public void SetSortingOrder() {
+		GetComponent<SpriteRenderer>().sortingOrder = GetSortingOrder();
+    }
+
+    public bool HasEntity() {
+        return entityRef != null;
+    }
+
+    public void PlaceEntity(Entity entity, Vector2 offset) {
+        entityRef = new EntityRef(entity, offset);
+        entity.SetSortingOrder(GetSortingOrder());
+    }
+}
