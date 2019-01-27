d8e83a95ed46827df412667dc776fc442e944456
Yilun Zhao <yl4zhao@uwaterloo.ca>
2019-01-27 14:45:19 -0800

    Eating regen

--- GGJ2019/Assets/Script/Items/Item.cs
+++ GGJ2019/Assets/Script/Items/Item.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+	public Player player;
     public string key;
     public Sprite sprite;
     public virtual bool usable { get; set; }
 
-    public virtual void Use() { }
+    public virtual void Use() { 
+    	if(player != null) {
+    		player.IncreaseHunger(2f);
+    	}
+    }
 }
